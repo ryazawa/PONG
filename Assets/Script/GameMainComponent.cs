ef526198f3711d5c27787c4781182cf00eff0746
S1321158\1321158 <klsh72@yahoo.co.jp>
2017-04-21 02:00:41 +0900

    弾の種類の追加

--- Assets/Script/GameMainComponent.cs
+++ Assets/Script/GameMainComponent.cs
@@ -19,7 +19,9 @@ public class GameMainComponent : MonoBehaviour {
 	[Header("ボール用パーティクルオブジェクト")]
 	[SerializeField] private ParticleSystem particle;
 
-	private Vector3 speed = new Vector3 (0f, 1.3f ,0f);
+	private Vector3 speed = new Vector3 (3f, 1.3f ,0f);
+
+	private float force = 0.05f;
 
 	private int min=0;
 	private float sec=0;
@@ -78,13 +80,15 @@ public class GameMainComponent : MonoBehaviour {
 		Vector3 position = player1.transform.position;
 
 		if (Input.GetKey (KeyCode.W)) {
-			//if (position.y <= 23f) {
+			if (position.y <= 23f) {
 				position.y += speed.y;
-			//}
+			}
+//			position.y += player1.GetComponent<Rigidbody2D>().velocity.y * 2;
 		} else if (Input.GetKey (KeyCode.S)) {
-			//if (position.y >= -26f) {
+			if (position.y >= -23f) {
 				position.y -= speed.y;
-			//}
+			}
+//			player1.GetComponent<Rigidbody2D>().AddForce(-transform.up * force);
 		}
 
 		player1.transform.position = position;
@@ -97,21 +101,22 @@ public class GameMainComponent : MonoBehaviour {
 		Vector3 position = player2.transform.position;
 
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			//if (position.y <= 23f) {
+			if (position.y <= 23f) {
 				position.y += speed.y;
-			//}
+			}
 		} else if (Input.GetKey (KeyCode.DownArrow)) {
-			//if (position.y >= -26f) {
+			if (position.y >= -23f) {
 				position.y -= speed.y;
-			//}
-		}
+			}
 
 		player2.transform.position = position;
 
+		}
 	}
 
 	private IEnumerator Shoot()
 	{
+		int shootType = Random.Range(0,3);
 
 		ScoreCount.isCol = false;
 		particle.Simulate (0.0f, true, false);
@@ -122,13 +127,23 @@ public class GameMainComponent : MonoBehaviour {
 				new ParticleSystem.Burst(0.0f,1)
 			});
 
-		//particle.gravityModifier = 10f;
-
-		//particle.startSpeed = 100f;
+		if (shootType == 0)
+			particle.gravityModifier = 10f;
+		else if (shootType == 1)
+			particle.startSpeed = 100f;
+		else if (shootType == 2)
+			particle.gravityModifier = -10f;
 
 		yield return new WaitForSeconds (1.5f);
 
 		particle.Play ();
+
+		yield return new WaitForSeconds (3f);
+
+		if (shootType == 0)
+			particle.gravityModifier = Random.Range (0, 30);
+		else if (shootType == 2)
+			particle.gravityModifier = Random.Range (0, -30);
 
 	}
 
